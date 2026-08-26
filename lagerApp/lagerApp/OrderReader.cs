c10a2b8cb1761049efcc5f0bf551ee470df255d8
Nikolai <nikolai.koop@norgesgruppen.no>
2026-08-26 14:38:59 +0200

    Added method to read orders from csv files

--- lagerApp/lagerApp/OrderReader.cs
+++ lagerApp/lagerApp/OrderReader.cs
@@ -18,12 +18,40 @@ public class OrderReader: IReader
             List<OrderLine> orderItems = orderLines.Skip(1).Select(o => new
                 {
                     Item = ReturnItemAsAction(o[0]),
-                    Quantity = GetQuantityAsAction(o[1]),
+                    Quantity = GetQuantityAsAction(o[1])
                 })
                 .Where(x => x.Item is not null && x.Quantity is not null)
                 .Select(o => new OrderLine(o.Item,o.Quantity)) 
                 .ToList();
             return new Order(orderItems);
+        }
+
+        return null;
+    }
+
+    public List<Item> ReadInventoryCsv(string filePath)
+    {
+        // Use the IncomingOrder.csv
+        List<string[]> orderLines = IReader.ReadCsvLines(filePath);
+    
+        // Split the file using simple syntax
+
+        string[]? orderHeader = orderLines.FirstOrDefault();
+        
+        if (orderHeader is not null)
+        {
+            List<Item> orderItems = orderLines.Skip(1).Select(o => new
+                {
+                    Item = ReturnItemAsAction(o[0]),
+                    Quantity = GetQuantityAsAction(o[1]),
+                    shelf = o[2]
+                })
+                .Where(x => x.Item is not null && x.Quantity is not null)
+                .SelectMany(
+                    x => Enumerable.Range(0, x.Quantity.Value),
+                    (x, _) =>new Item(x.shelf, x.Item)) 
+                .ToList();
+            return orderItems;
         }
 
         return null;
